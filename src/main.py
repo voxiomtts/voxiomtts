b9bdd6917d2b5c7839b14cbf94519f19c61d9888
voxiomtts <voxiomtts@outlook.com>
2025-07-26 07:52:35 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from src.gui.main_window import MainWindow
+from gui.main_window import MainWindow
 
-def main():
+if __name__ == "__main__":
     app = QApplication(sys.argv)
+    
+    # Load stylesheet if needed
+    with open("gui/assets/styles.qss", "r") as f:
+         app.setStyleSheet(f.read())
+    
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
-
-if __name__ == "__main__":
-    main()
