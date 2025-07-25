f39d49e0afc7cf61eff17ef4d127a37991d0fcb4
voxiomtts <voxiomtts@outlook.com>
2025-07-26 03:56:39 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from gui.main_window import MainWindow
+from src.gui.main_window import MainWindow
 
 def main():
     app = QApplication(sys.argv)
