5bbc97cc79ae802bf3d6843bb4b6079b45bb93a4
voxiomtts <voxiomtts@outlook.com>
2025-07-26 09:02:13 +0800

    Create hook-PySide6.py

---
+++ hooks/hook-PySide6.py
@@ -0,0 +1,8 @@
+# hooks
+from PyInstaller.utils.hooks import collect_all
+
+def hook(hook_api):
+    datas, binaries, hiddenimports = collect_all('PySide6')
+    hook_api.add_datas(datas)
+    hook_api.add_binaries(binaries)
+    hook_api.add_imports(*hiddenimports)
