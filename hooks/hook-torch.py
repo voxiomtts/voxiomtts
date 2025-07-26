5e0d6b09ea2144631b7d1daa1408cbfff090bf78
voxiomtts <voxiomtts@outlook.com>
2025-07-26 13:22:54 +0800

    Update hook-torch.py

--- hooks/hook-torch.py
+++ hooks/hook-torch.py
@@ -1,16 +1,10 @@
-from PyInstaller.utils.hooks import collect_all, collect_data_files
+# Create file 'hooks/hook-torch.py'
+from PyInstaller.utils.hooks import collect_all
 
 def hook(hook_api):
-    # Main torch dependencies
-    datas, binaries, hiddenimports = collect_all('torch')
-    
-    # Add Silero-specific dependencies
-    datas += collect_data_files('sentencepiece')
-    datas += collect_data_files('omegaconf')
-    
-    hook_api.add_datas(datas)
-    hook_api.add_binaries(binaries)
-    hook_api.add_imports(*hiddenimports + [
-        'torch._C._distributed_c10d',
-        'numpy.random.common'
-    ])
+    packages = ['torch', 'numpy', 'sounddevice']
+    for package in packages:
+        datas, binaries, hiddenimports = collect_all(package)
+        hook_api.add_datas(datas)
+        hook_api.add_binaries(binaries)
+        hook_api.add_imports(*hiddenimports)
